e47635371b9ea7fca8dff3f082df4c7f644da402
royFloresNyc <royfloresnyc@gmail.com>
2021-01-25 17:41:33 -0500

    Routes for dishes/:dishId

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ const express = require('express'),
     http = require('http'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
-    dishRouter = require('./routes/dishRouter')
+    dishRouter = require('./routes/dishRouter');
 
-const hostname= 'localhost'
-const port = 3000
+const hostname= 'localhost';
+const port = 3000;
 
-const app = express()
-app.use(morgan('dev'))
-app.use(bodyParser.json())
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-app.use('/dishes', dishRouter)
+app.use('/dishes', dishRouter);
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>')
-})
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`)
-})
+    console.log(`Server running at http://${hostname}:${port}`);
+});
